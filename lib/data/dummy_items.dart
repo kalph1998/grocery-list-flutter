92d7acd912ff594e6e19737d721ce0a92723c421
Kalph upadhyay <kalph.upadhyay@intimetec.com>
2023-06-20 11:09:12 +0530

    added modal

---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,11 @@
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+
+final groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+];
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,30 +10,44 @@ class Home extends StatelessWidget {
         title: const Text('Your groceries'),
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        padding: EdgeInsets.symmetric(horizontal: 10),
         child: ListView(
           children: [
-            Row(
-              children: [
-                Container(
-                  width: 20,
-                  height: 20,
-                  color: Colors.amber,
-                ),
-                SizedBox(width: 20),
-                Text(
-                  'Milk',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                ),
-                Spacer(),
-                Text(
-                  '1',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                )
-              ],
-            )
+            GroceryItem(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class GroceryItem extends StatelessWidget {
+  const GroceryItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Row(
+        children: [
+          Container(
+            width: 20,
+            height: 20,
+            color: Colors.amber,
+          ),
+          SizedBox(width: 20),
+          Text(
+            'Milk',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+          Spacer(),
+          Text(
+            '1',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          )
+        ],
       ),
     );
   }
